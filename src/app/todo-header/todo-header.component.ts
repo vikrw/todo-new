690c176e5757b52bcc2dd994362b1304a451b4d8
YOUR_USERNAME <deb@itobuz.com>
2017-09-10 10:01:00 +0530

    todo-header

---
+++ src/app/todo-header/todo-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-header',
+  templateUrl: './todo-header.component.html',
+  styleUrls: ['./todo-header.component.sass']
+})
+export class TodoHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
