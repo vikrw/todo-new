b4fbbbfb5bc5ac14c9cc5d882cdc03a340db11a9
Prosenjit Manna <developer.prosenjit@gmail.com>
2017-09-10 15:39:46 +0530

    fix sass config issue

--- src/app/todo-header/todo-header.component.ts
+++ src/app/todo-header/todo-header.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-header',
   templateUrl: './todo-header.component.html',
-  styleUrls: ['./todo-header.component.sass']
+  styleUrls: ['./todo-header.component.scss']
 })
 export class TodoHeaderComponent implements OnInit {
 
